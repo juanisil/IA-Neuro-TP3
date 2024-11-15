78a55b14123c7d6e35d84d6dd929b3b921e4b59f
Juan Silvestri <58190785+juanisil@users.noreply.github.com>
2024-11-15 16:37:05 -0300

    Feat: tokenize method

--- n-grams.py
+++ n-grams.py
@@ -5,27 +5,36 @@ from nltk import word_tokenize
 from collections import Counter
 
 Corpus = List[str]
-Token = str
 
 class Ngram:
     def __init__(self, n:int):
         self.n = n
+        # Matriz de MxP donde M=numero de (N-1)-gramas del corpus 
+        # y P=size del vocabulario
         self.frequency_table = pd.DataFrame()
+        self.vocav = set()
 
     def fit(self, corpus:Corpus):
         all_n_grams = []
+
         for text in corpus:
-            tokens: Token = word_tokenize(text.lower())
+            tokens = self.tokenize(text)
             n_grams = self.extract_ngrams(tokens)
             all_n_grams.append(n_grams)
 
         # reemplazar por np.array
-        self.frequency_table = pd.DataFrame(Counter(all_n_grams).items(), columns=['N-gram', 'Frequency'])
+        self.frequency_table = None
 
 
-    def extract_ngrams(self, text):
+    def extract_ngrams(self, tokens):
         # Extract all the self.n-grams of the text.
         pass
+
+    def tokenize(self, text):
+        text = text.lower()
+        text = text.split(" ")
+        text = ["<s>"] * (self.n-1) + text + ["</s>"]
+        return text
     
     def predict(self, prompt):
         # return the token with highest probability given n-1 tokens

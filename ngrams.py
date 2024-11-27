705d60da68e75b1d1ed1cdc1f9368b35d4069ad9
IgnacioPardo <ignacio.pardo@ort.edu.ar>
2024-11-27 08:16:28 -0300

    feat(friends)

--- ngrams.py
+++ ngrams.py
@@ -6,14 +6,18 @@ from collections import Counter
 from tqdm import tqdm
 
 Corpus = List[str]
-START = "<s>"
-END = "</s>"
+SENTENCE_START = "<l>"
+SENTENCE_END = "</l>"
 
+SCENE_START = "<s>"
+SCENE_END = "</s>"
+
+EPISODE_START = "<e>"
+EPISODE_END = "</e>"
 
 def corpus_from_file(file_path: str) -> Corpus:
     with open(file_path, "r") as file:
         return file.readlines()
-
 
 class Ngram:
     def __init__(self, n: int):
@@ -22,20 +26,46 @@ class Ngram:
         self.frequency_table = None
         self.vocav = set()
 
-    def fit(self, corpus: Corpus, verbose=False):
+    def fit(self, corpus: Corpus, verbose=False, type="text"):
+        """
+        Types are:
+            text: for normal text
+            script: for script, with scene and episode markers
+        """
         all_n_grams = []
+        all_tokens = []
 
         iterator = corpus
         if verbose:
             iterator = tqdm(iterator, total=len(corpus))
             print("Tokenizing corpus")
+        scene_started = False
+        episode_started = False
         for text in iterator:
+            if text == "\n":
+                continue
             tokens = self.tokenize(text)
+            if not episode_started:
+                tokens = [EPISODE_START] + tokens
+                episode_started = True
+            if ("[scene" in text) or ("[Scene" in text):
+                if scene_started:
+                    tokens = tokens + [SCENE_END]
+                tokens = [SCENE_START] + tokens
+                scene_started = True
+            if "\nEnd\n" in text:
+                tokens = tokens + [SCENE_END, EPISODE_END]
+                scene_started = False
+                episode_started = False
+
             n_grams = self.extract_ngrams(tokens, verbose)
             all_n_grams.append(n_grams)
+            all_tokens.append(tokens)
 
         self.frequency_table = None
         self.create_frequency_table(all_n_grams, verbose)
+
+        return all_tokens
 
     def extract_ngrams(self, tokens, verbose=False):
         # Extract all the self.n-grams of the text.
@@ -45,7 +75,7 @@ class Ngram:
             iterator = tqdm(iterator, total=len(tokens) - self.n + 1)
             print("Extracting n-grams")
         for i in iterator:
-            n_grams.append(tuple(tokens[i:i + self.n]))
+            n_grams.append(tuple(tokens[i : i + self.n]))
 
         return n_grams
 
@@ -86,7 +116,7 @@ class Ngram:
     def tokenize(self, text):
         text = text.lower()
         text = text.split(" ")
-        text = [START] * (self.n - 1) + text + [END]
+        text = [SENTENCE_START] * (self.n - 1) + text + [SENTENCE_END]
         return text
 
     def predict(self, tokenized_context: str) -> str:
@@ -99,27 +129,32 @@ class Ngram:
         logits = self.frequency_table[n_minus_1_gram]
         return logits
 
-    def generate(self, context: Tuple[str]="", temperature: float=0.9) -> str:
+    def generate(
+        self, context: Tuple[str] = "", temperature: float = 0.9
+    ) -> str:
         max_length = 30
         answer = ""
         if context == "":
-            context = [START] * (self.n - 1)
+            context = [SENTENCE_START] * (self.n - 1)
         else:
             context = self.tokenize(context)[:-1]
         next_word = ""
-        while(next_word != END and max_length>0):
+        while next_word != SENTENCE_END and max_length > 0:
             logits = self.predict(context)
             if logits is None:
                 break
+
             logits = {k: v for k, v in logits.items() if v > 0}
             logits = {k: v ** (1 / temperature) for k, v in logits.items()}
             logits = {k: v / sum(logits.values()) for k, v in logits.items()}
             next_word = np.random.choice(list(logits.keys()), p=list(logits.values()))
             answer += f" {next_word}"
-            context = context[1:] + [next_word,]
+            context = context[1:] + [
+                next_word,
+            ]
             max_length -= 1
 
-        return answer[1:-len(END)]
+        return answer
 
         # next_word = ""
         # while(next_word != END and max_length>0):
@@ -130,6 +165,31 @@ class Ngram:
 
         # return answer[1:-len(END)]
 
+    def generate_scene(self, context: Tuple[str] = "", temperature: float = 0.9, max_sentences=10) -> str:
+        scene = context
+        while SCENE_END not in scene and max_sentences > 0: 
+            sentence = self.generate(context, temperature)
+            if SCENE_START in sentence:
+                break
+            scene += sentence
+            context = sentence[-2:]
+
+            max_sentences -= 1
+
+        return scene
+
+    def generate_episode(self, context: Tuple[str] = "", temperature: float = 0.9, max_scenes=10, max_lines_per_scene=10) -> str:
+        episode = context
+        while EPISODE_END not in episode and max_scenes > 0: 
+            sentence = self.generate_scene(context, temperature, max_lines_per_scene)
+            if EPISODE_START in sentence:
+                break
+            episode += sentence
+            context = sentence[-2:]
+
+            max_scenes -= 1
+
+        return episode
 
     def perplexity(self, text: Corpus) -> float:
         # Calculate the perplexity of the text given the n-gram model.
